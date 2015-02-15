8cc49f64aac6a21163139e0aaf18233b1111acd6
Davis Cook <daviscook447@gmail.com>
2015-02-14 19:47:35 -0500

    working on footer

--- app/modules/main-student/main-student-ctrl.js
+++ app/modules/main-student/main-student-ctrl.js
@@ -1,6 +1,6 @@
 angular.module('starter.main-student', [])
 
-.controller('MainStudentCtrl', function($scope, $ionicModal, $state, $timeout) {
+.controller('MainStudentCtrl', function($scope, $ionicPopup, $ionicModal, $state, $timeout) {
 
   $scope.hello="Hello World!";
 
@@ -9,7 +9,23 @@ angular.module('starter.main-student', [])
   }, 3000);
 
   $scope.goSession = function() {
-    $state.go('app.student');
+    //TODO: validate that the session exists
+    //.then(function() {code goes here}
+    var validated = true;
+    if (validated) {
+      $ionicPopup.alert({
+        title: "Class found!",
+      }).then(function(done) {
+        $state.go('app.student');
+      });
+    } else {
+      $ionicPopup.alert({
+        title: "Class ID already taken! Try another one or add some numbers to the end of yours!"
+      }).then(function(done) {
+
+      });
+    }
+
   }
 
 });
--- app/modules/student/student-ctrl.js
+++ app/modules/student/student-ctrl.js
@@ -2,6 +2,9 @@ angular.module('starter.student', ['starter.services.f'])
 
 .controller('StudentCtrl', function($scope, $state, $stateParams, $firebaseAuth, $firebase, $f, $ionicModal) {
 
-
+  $scope.exitSession = function() {
+    //TODO: exit class code
+    $state.go('app.main-student')
+  }
 
 });
